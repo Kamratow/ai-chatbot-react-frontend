a6aa6ae0f58c1fffd4430517065b28e25ee0b796
Kamil Ratowski <kamilratowski90@gmail.com>
2024-09-07 23:46:54 +0200

    Created some basic implemantation for react ai chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,114 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import 'dotenv/config';
+import { useState } from "react";
+import "./App.css";
+import OpenAI from "openai";
+
+import classes from "./App.module.css";
+
+const openai = new OpenAI({
+  organization: import.meta.env.VITE_OPENAI_ORGANIZATION_ID,
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState<string>("");
+  const [chats, setChats] = useState<OpenAI.ChatCompletionMessageParam[]>([]);
+  const [isTyping, setIsTyping] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  const chat = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const oldChats = [...chats];
+
+    const newUserMessage: OpenAI.ChatCompletionUserMessageParam = {
+      role: "user",
+      content: message,
+    };
+
+    const newChats: OpenAI.ChatCompletionMessageParam[] = [
+      ...chats,
+      newUserMessage,
+    ];
+
+    setIsTyping(true);
+    setChats(newChats);
+
+    await openai.chat.completions
+      .create({
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are a helpful assistant. Try to be concise and straight to the point.",
+          },
+          ...newChats,
+        ],
+        model: "gpt-4o-mini",
+      })
+      .then((result) => {
+        console.log(result.choices[0].message.content);
+        setChats([...newChats, result.choices[0].message]);
+      })
+      .catch((error) => {
+        setChats(oldChats);
+        setIsError(true);
+        console.log(error);
+      })
+      .finally(() => {
+        setIsTyping(false);
+        setMessage("");
+      });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <h1>React Chatbot connected with ChatGPT</h1>
+
+      <div className={classes.chatContainer}>
+        {chats &&
+          chats.length > 0 &&
+          chats.map((chat, index) => (
+            <p
+              key={index}
+              className={
+                chat.role === "assistant"
+                  ? classes.assistantMessage
+                  : classes.userMessage
+              }
+            >
+              <span className={classes.roleText}>{chat.role}</span>
+              <span>: </span>
+              <span>{chat.content as string}</span>
+            </p>
+          ))}
+
+        {!isTyping && (
+          <div className={classes.typingNotificationWrapper}>
+            <p>
+              <i>Typing...</i>
+            </p>
+          </div>
+        )}
+
+        {!isError && (
+          <div className={classes.errorNotificationWrapper}>
+            <p>Error while getting an answer - please try again</p>
+          </div>
+        )}
+        <form onSubmit={(e) => chat(e)}>
+          <input
+            type="text"
+            name="message"
+            value={message}
+            placeholder="Type a message and hit enter"
+            onChange={(e) => setMessage(e.target.value)}
+            className={classes.chatTextInput}
+          />
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
